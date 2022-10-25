624d1d40ffdfc6e9f08233c884958f6464201dce
Dor Mizrahi <dorm@h-MacBook-Pro-sl-Dor.local>
2022-10-25 16:46:00 +0300

    hey

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
 from flask import Flask, jsonify, request
-products = [
-    {'id': 1, 'name': 'Product 1'},
-    {'id': 2, 'name': 'Product 2'}
-]
+from parcinsson_Project_ready import *
+
+
 app = Flask(__name__)
-# curl -v http://localhost:8080/products
-@app.route('/products')
-def get_products():
-    return jsonify(products)
-
-# curl -v http://localhost:8080/product/1
-@app.route('/product/<int:id>')
-def get_product(id):
-    product_list = [product for product in products if product['id'] ==id]
-    if len(product_list) == 0:
-        return f'Product with id {id} not found', 404
-    return jsonify(product_list[0]) 
+# curl -v http://localhost:8080/ui
+@app.route('/ui', methods=['GET'])
+def get_ui():
+    ui=parcinsson_Project_ready.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    #sys.exit(app.exec())
+    return MainWindow.show()
 
 
-# curl --header "Content-Type: application/json" --request POST --data '{"name": "Product 3"}' -v http://localhost:8080/product
-@app.route('/product', methods=['POST'])
-def post_product():
+
+
+import numpy as np
+# curl --header "Content-Type: application/json" --request POST --data '{"name": "Product 3"}' -v http://localhost:8080/checksick
+@app.route('/checksick', methods=['POST'])
+def post_sick():
+    
     # Retrieve the product from the request body
-    request_product = request.json
-    # Generate an ID for the post
-    new_id = max([product['id'] for product in products]) + 1
-    # Create a new product
-    new_product = {
-        'id': new_id,
-        'name': request_product['name']
-    }
-    # Append the new product to our product list
-    products.append(new_product)
-    # Return the new product back to the client
-    return jsonify(new_product), 201
+    request_data_sick = request.json # list with all the data
+    # np.array([float(self.mdvpfo.toPlainText()), float(self.mdvpflo_hz.toPlainText()), float(self.mdvp_hitterpr.toPlainText()), float(self.jitterAbs.toPlainText()), float(self.mdvpRAP.toPlainText()), float(self.mdvpppq.toPlainText()),float(self.mdvpDDP.toPlainText()),float(self.mdvpSHIMMER.toPlainText()),float(self.mdvp_SHIMMERDB.toPlainText()), float(self.SHIMMERAPQ3.toPlainText()),float(self.SHIMMERAPq5.toPlainText()), float(self.MDVPAPQ.toPlainText()),float(self.SHIMMERDDA.toPlainText()),float(self.HNR.toPlainText()), float(self.RPDE.toPlainText()),float(self.DFA.toPlainText()),float(self.SPREAD1.toPlainText()),float(self.SPREAD2.toPlainText()),float(self.D2.toPlainText()),float(self.PPE.toPlainText())]).reshape(1, -1) # X is a matrix of features
+    Ui_MainWindow()
+    ui, sick = Ui_MainWindow()
+    check_sick = request_data_sick.reshape(1, -1)
+
+    if model.predict(check_sick)==[1]:
+                print("yes")
+                from PyQt6.QtWidgets import QMessageBox
+                msg=QMessageBox()
+                msg.setWindowTitle("resault")
+                msg.setText("patient is sick")
+                x=msg.exec()
+                return jsonify({'message': 'patient is sick'})
+    else:
+                from PyQt6.QtWidgets import QMessageBox
+                msg=QMessageBox()
+                msg.setWindowTitle("resault")
+                msg.setText("patient  isn't sick")
+                x=msg.exec()
+                return jsonify({'message': 'patient  isn"t sick'})
+        
+  
 
 # curl -- header "Content-Type: application/json" --request PUT --data'{"name": "Updated Product 2"}' -v http://localhost:8080/product/2
 @app.route('/product/<int:id>', methods=['PUT'])
